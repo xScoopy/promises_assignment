428a075633bcd99f9927299e3a6c6c805628bca9
xScoopy <69348168+xScoopy@users.noreply.github.com>
2022-04-05 00:07:03 -0500

    challenge 1 done

--- Promise-1/index.js
+++ Promise-1/index.js
@@ -2,7 +2,9 @@
 const p = new Promise((resolve, reject) => {
   setTimeout(() => {
     // reject("--- Oops ---");
-    resolve('>>> Success! <<<');
+    resolve('its all good');
+    resolve('still all good')
+    
   }, 2000);
 });
 
@@ -17,9 +19,41 @@ p.then((message) => {
 // **Problems to solve**
 
 // **1)** What happens when a promise is rejected? Test it by calling `reject()`
+//The catch block is executed instead of the try. 
+// const p = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     // reject("--- Oops ---");
+//     reject('error');
+//   }, 2000);
+// });
 
 // **2)** What happens when you call both `resolve` and `reject`? Test this.
+//resolved code runs, reject does not. 
+//const p = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     // reject("--- Oops ---");
+//     resolve('resolved!')
+//     reject('error');
+//   }, 2000);
+// });
 
 // **3)** Does the order matter you call resolve and reject matter? Test this. 
-
+//it seems it does matter. the reject was executed when it was the first resolution in the promise.  
+// const p = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     // reject("--- Oops ---");
+//     reject('error');
+//     resolve('resolved!')
+    
+//   }, 2000);
+// });
 // **4)** What happens if you call `resolve` or `reject` more than once? Test this out for yourself.
+//seems that only the first resolution or rejection is accepted. the others are ignored. 
+// const p = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     // reject("--- Oops ---");
+//     resolve('its all good');
+//     resolve('still all good')
+    
+//   }, 2000);
+// });
