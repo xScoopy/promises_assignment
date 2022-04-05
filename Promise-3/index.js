e2d92583aaf942cb3e074ced6f01526609c3e69d
xScoopy <69348168+xScoopy@users.noreply.github.com>
2022-04-05 00:37:01 -0500

    challenge 3 done

--- Promise-3/index.js
+++ Promise-3/index.js
@@ -28,16 +28,24 @@ function uppercaser(str) {
 // Notice how we chain these promises together. The 
 // Result of one is then passed to the next. 
 
-greet('Your name') // Returns a Promise
+greet('Jeremiah') // Returns a Promise
 .then(str => uppercaser(str))  // Upper case the results from greet() Returns a Promise
 .then(str => console.log(str)) // Log the results of uppercaser()
 .catch(err => console.log(err)) // Catches an error
 
 // Challenges: get greet() to fail by passing a non string value
-// What happens? 
-
+// What happens? it gets rejected and the chain is broken
+greet(7)
+.then(str => uppercaser(str))  // Upper case the results from greet() Returns a Promise
+.then(str => console.log(str)) // Log the results of uppercaser()
+.catch(err => console.log(err))
 // Challenge: get uppercaser() to fail by passing a non string value
 // What happens? 
+uppercaser(7)
+.then((str) => {
+  console.log(str)
+})
+.catch(err => console.log(err))
 
 // Challenge: Notice there is only a single .catch() at the end. 
 // Explain the behavior?
